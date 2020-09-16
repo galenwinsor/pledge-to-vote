e5318018efd30c7fc3c40dbfabd3077fb6afa05d
galenlwinsor <galen@jaimeharrison.com>
2020-09-16 17:10:30 -0400

    Finished first mock-up of form and share pages

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,6 @@
 // enabling buttons based on form contents
 $('document').ready(function() {
 
-  $('#container').addClass('fade');
-
   $('input[id="other-reason"]').change(function() {
     if ($('input[id="other-reason"]:checked').val()) {
       $('#other-input').removeAttr('disabled');
@@ -45,11 +43,26 @@ $('document').ready(function() {
   $('label[name="how-label"]').click(function() {
     checkedHow($(this));
   })
+
+  // preventing return submitting form
+  $('form input').keydown(function (e) {
+    if (e.keyCode == 13) {
+        e.preventDefault();
+        return false;
+    }
+});
+
+  $('button[type="submit"]').click(function() {
+    if (checkZip($('#zip').val())) {
+      setStorage();
+      goShare();
+    }
+  })
 });
 
 function next(next, checkbox) {
-  $(next).show();
-  $(checkbox).css('display','block');
+  $(next).css('display','flex');
+  $(checkbox).show();
   $('html,body').delay(500).animate({
     scrollTop: $(next).offset().top
   }, 1400);
@@ -63,4 +76,24 @@ function checkedReason(label) {
 function checkedHow(label) {
   $('label[name="how-label"]').removeClass('chosen');
   label.addClass('chosen');
+}
+
+function checkZip(zip) {
+  if (isNaN(zip)) {
+    alert("Please enter a valid zip code.");
+    return false;
+  } else if (zip < 29001 || zip > 29945) {
+    alert("Please enter a valid South Carolina zip code");
+    return false;
+  } else return true;
+}
+
+function goShare() {
+  $(location).attr('href','share.html');
+}
+
+function setStorage() {
+  sessionStorage.setItem('name', $('#first-name').val() + ' ' + $('#last-name').val());
+  sessionStorage.setItem('reason', $('input[name="reason"]').val());
+  sessionStorage.setItem('how', $('input[name="vote-method"]').val());
 }
